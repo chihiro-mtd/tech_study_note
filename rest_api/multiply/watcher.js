a5126d4924174e385994bcb1bb187ea20f458a10
chihiro-mtd <chihiro.m@asial.co.jp>
2016-08-12 18:39:30 +0900

    データの伝達経路を修正

--- rest_api/multiply/watcher.js
+++ rest_api/multiply/watcher.js
@@ -1,7 +1,21 @@
-var req = new XMLHttpRequest();
-req.open('GET', "maker.php", true);
-req.onreadystatechange = function() {
-    var disp = document.getElementById("disp");
-    disp.innerHTML = req.responseText;
-};
-req.send("");
+function getNum() {
+    var form_data = new FormData();
+
+    var num1 = document.forms.myForm.id_num1.value;
+    var num2 = document.forms.myForm.id_num2.value;
+
+    form_data.append("num1", num1);
+    form_data.append("num2", num2);
+
+    hitAPI(form_data);
+}
+
+function hitAPI(form_data) {
+    var req = new XMLHttpRequest();
+    req.open('POST', "maker.php", true);
+    req.send(form_data);
+    req.onreadystatechange = function() {
+        var disp = document.getElementById("disp");
+        disp.innerHTML = req.responseText;
+    };
+}
