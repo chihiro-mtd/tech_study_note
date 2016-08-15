fcdfe55f7ad63a6f8c7c5decd1e41105b7bc280a
chihiro-mtd <chihiro.m@asial.co.jp>
2016-08-15 17:52:34 +0900

    id番号を表示するよう修正

--- crud_memo_app/memo_app.js
+++ crud_memo_app/memo_app.js
@@ -1,16 +1,16 @@
 httpObj = new XMLHttpRequest();
 httpObj.open("get", "./memo.json", true);
 httpObj.onload = function() {
-    var div_disp = document.getElementById("disp");
+    var disp = document.getElementById("disp");
 
     var myData = JSON.parse(this.responseText);
     var txt = "";
     for (var i = 0; i < myData.Memo.length; i++) {
-        txt = txt + "<a href='example.html'>" + myData.Memo[i].title + "</a> " + myData.Memo[i].text +
+        txt = txt + i + " - <a href='example.html'>" + myData.Memo[i].title + "</a> " + myData.Memo[i].text +
         " <a href='javascript:void(0);' onclick='deleteData(" + i + ");'>削除</a><br>" ;
     }
 
-    div_disp.innerHTML = txt;
+    disp.innerHTML = txt;
 }
 httpObj.send("");
 
