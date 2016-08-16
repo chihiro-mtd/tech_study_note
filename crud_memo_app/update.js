c815f138ddb2f95f5cd2ef4953ac60acf6db2475
chihiro-mtd <chihiro.m@asial.co.jp>
2016-08-16 15:37:41 +0900

    更新機能を実装

--- crud_memo_app/update.js
+++ crud_memo_app/update.js
@@ -5,7 +5,7 @@ function getParameter() {
     var kv = tmp_parameter.split('=');
     var index = kv[1];
 
-    getTitleAndTextFromJSON(index);
+    return index;
 }
 
 // JSONファイルから更新対象の要素を取得
@@ -20,27 +20,45 @@ function getTitleAndTextFromJSON(index) {
         var title = myData['Memo'][index]['title'];
         var text = myData['Memo'][index]['text'];
 
-        console.log(title);
-
         setTitleAndText(title, text);
     }
     httpObj.send("");
 }
 
+// タイトルと内容を入力欄にセット
 function setTitleAndText(title, text) {
     document.forms.myForm.id_title.value = title;
     document.forms.myForm.id_text.value = text;
 }
 
+// フォームの入力値を取得
+function getFormValue() {
+    var form_data = new FormData();
+
+    var index = getParameter();
+    var title = document.forms.myForm.id_title.value;
+    var text = document.forms.myForm.id_text.value;
+
+    form_data.append("index",index);
+    form_data.append("title", title);
+    form_data.append("text", text);
+
+    hitAPI(form_data);
+}
+
+// phpにフォームの入力値を投げる
 function hitAPI(form_data) {
     var httpObj = new XMLHttpRequest();
-    httpObj.open('POST', "register.php", true);
+    httpObj.open('POST', "update.php", true);
     httpObj.send(form_data);
     httpObj.onload = function() {
+      //var update_status = document.getElementById("update_status");
+      //update_status.innerHTML = httpObj.responseText;
+
         if (httpObj.responseText == "OK") {
             location.href = "./register_ok.html";
         } else {
-            var register_status = document.getElementById("register_status");
+            var update_status = document.getElementById("register_status");
             register_status.innerHTML = "<h2>NG</h2>";
         }
     };
---
+++ crud_memo_app/update.php
@@ -0,0 +1,27 @@
+<?php
+
+$index = $_POST['index'];
+$title = $_POST['title'];
+$text = $_POST['text'];
+
+if (mb_strlen($title, 'UTF-8') <= 10 && mb_strlen($text, 'UTF-8') <= 30) {
+    $filename = 'memo.json';
+    $handle = fopen($filename, 'r');
+
+    $memo_list = json_decode(fread($handle, filesize($filename)), true);
+    fclose($handle);
+
+    $new_array = array('id' => $num_id, 'title' => $title, 'text' => $text);
+
+
+    $memo_list['Memo'][$index]['title'] = $title;
+    $memo_list['Memo'][$index]['text'] = $text;
+
+    $handle = fopen($filename, 'w');
+    fwrite($handle, json_encode($memo_list, JSON_UNESCAPED_UNICODE));
+    fclose($handle);
+
+    echo 'OK';
+} else {
+    echo 'NG';
+}
