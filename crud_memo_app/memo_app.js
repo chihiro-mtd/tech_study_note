42c2b8642721402838cd7d3eb049a7ba9d0e9def
chihiro-mtd <chihiro.m@asial.co.jp>
2016-08-16 16:02:45 +0900

    tableで表示を整理

--- crud_memo_app/memo_app.js
+++ crud_memo_app/memo_app.js
@@ -4,11 +4,13 @@ httpObj.onload = function() {
     var disp = document.getElementById("disp");
 
     var myData = JSON.parse(this.responseText);
-    var txt = "";
+    var txt = "<table border='1'><tr><th>表示順</th><th>ID</th><th>タイトル(クリックで更新)</th><th>内容</th><th>操作</th></tr>";
     for (var i = 0; i < myData.Memo.length; i++) {
-        txt = txt + i + " - <a href='update.html?index=" + i + "'>" + myData.Memo[i].title + "</a> " + myData.Memo[i].text +
-        " <a href='javascript:void(0);' onclick='deleteData(" + i + ");'>削除</a><br>" ;
+        txt = txt + "<tr><td>" +  i + "</td><td>" + myData.Memo[i].id + "</td><td><a href='update.html?index=" + i + "'>" + myData.Memo[i].title + "</a></td><td>"
+        + myData.Memo[i].text + "</td><td><a href='javascript:void(0);' onclick='deleteData(" + i + ");'>削除</a></td></tr>" ;
     }
+
+    txt = txt + "</table>";
 
     disp.innerHTML = txt;
 }
