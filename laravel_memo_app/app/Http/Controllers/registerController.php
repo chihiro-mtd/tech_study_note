a181a50cd8d241cf658af2a6eddcea476f524e5a
chihiro-mtd <chihiro.m@asial.co.jp>
2016-08-19 11:49:22 +0900

    jsonファイルへの書き込みを確認

--- laravel_memo_app/app/Http/Controllers/registerController.php
+++ laravel_memo_app/app/Http/Controllers/registerController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use File;
+
 class registerController extends Controller
 {
     public function index()
@@ -9,9 +11,27 @@ class registerController extends Controller
         return view('register');
     }
 
-    public function register(){
-      $input_value = \Request::all();
+    public function register()
+    {
+        $input_value = \Request::all();
+        $json_path = storage_path().'/json/memo.json';
 
-      return $input_value;
+        $title = $input_value['title'];
+        $text = $input_value['text'];
+
+        $json = file_get_contents($json_path);
+        $memo_list = json_decode($json, true);
+        $memo_list = $memo_list['Memo'];
+
+        $new_id = count($memo_list);
+        $new_memo = ['id' => $new_id, 'title' => $title,'text' => $text];
+
+        array_push($memo_list,$new_memo);
+
+        $memo_list = json_encode($memo_list, JSON_UNESCAPED_UNICODE);
+
+        $json_written = File::put($json_path,$memo_list);
+
+        return $memo_list;
     }
 }
